e3558d8e96f4c1f6d9f609eb413f90ba2e6a71a0
Michael Alao <contactleomax@gmail.com>
2018-11-07 22:37:17 +0100

    Update Service.java

--- app/src/main/java/com/example/android/fetchdeveloperprofile/api/Service.java
+++ app/src/main/java/com/example/android/fetchdeveloperprofile/api/Service.java
@@ -7,6 +7,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Service {
-    @GET("/search/users?q=language:java+location:lagos&per_page=100&sort=followers")
+    @GET("/search/users?q=language:java+location:lagos&per_page=50&sort=followers")
     Call<ItemResponse> getItems();
 }
